4398d0fa8d23948ea755a2efaaff19fd6a920bdf
Pierre Pronchery <khorben@defora.org>
2020-12-06 20:45:24 +0100

    edgebsd-update: support flags from rc.conf for the RC script

--- update/files/edgebsd-update.sh
+++ update/files/edgebsd-update.sh
@@ -15,10 +15,10 @@ stop_cmd=":"
 
 edgebsd_update_start()
 {
-	EDGEBSD_UPDATE="@PREFIX@/sbin/edgebsd-update -S"
+	EDGEBSD_UPDATE="@PREFIX@/sbin/edgebsd-update"
 	EDGEBSD_MIRROR="/var/cache/edgebsd-update"
 
-	[ ! -d "$EDGEBSD_MIRROR" ] || $EDGEBSD_UPDATE -M "$EDGEBSD_MIRROR"
+	[ ! -d "$EDGEBSD_MIRROR" ] || $EDGEBSD_UPDATE $edgebsd_update_flags -M "$EDGEBSD_MIRROR"
 }
 
 load_rc_config $name
